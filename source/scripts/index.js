730b9f4c3ebf0ea8b28ecc0383559040bfa1b0c1
Van1s1mys <malkovsin@gmail.com>
2023-12-14 16:18:56 +0500

    burger

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+var burger = document.querySelector('.burger');
+
+burger.addEventListener('click', function() {
+  if (this.classList.contains('burger--active')){
+    this.classList.remove('burger--active')
+  }
+  else{
+    this.classList.add('burger--active');
+  }
+});
+
