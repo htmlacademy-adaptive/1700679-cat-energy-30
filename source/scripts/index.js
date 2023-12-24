f5efab232bee8803cb607eeafc896444b3af14e9
Van1s1mys <malkovsin@gmail.com>
2023-12-24 23:26:19 +0500

    header mobile js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,9 +1,18 @@
 /* в этот файл добавляет скрипты*/
 const burger = document.querySelector('.burger');
+const nav = document.querySelector('.main-nav');
+
+if (nav.classList.contains('main-nav--nojs')) {
+  nav.classList.remove('main-nav--nojs');
+}
+
 burger.addEventListener('click', function() {
   if (this.classList.contains('burger--active')) {
     this.classList.remove('burger--active');
+    nav.classList.add('main-nav--closed');
   } else {
     this.classList.add('burger--active');
+    nav.classList.remove('main-nav--closed');
   }
 });
+
