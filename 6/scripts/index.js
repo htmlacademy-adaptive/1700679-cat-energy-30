b154f495b3decda0224e1426f832459f206c4103
keksobot <keksobot@users.noreply.github.com>
2023-12-24 07:10:47 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/1700679-cat-energy-30@30fa41a4c62bd6506088dde99531632d6a3923ad 🚀

--- 6/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var e=document.querySelector(".burger");e.addEventListener("click",function(){this.classList.contains("burger--active")?this.classList.remove("burger--active"):this.classList.add("burger--active")});
