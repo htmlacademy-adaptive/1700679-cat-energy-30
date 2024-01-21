d89068ee79d696775dffb0dfa2c29d0aff080664
Van1s1mys <malkovsin@gmail.com>
2024-01-21 20:58:24 +0500

    catalog adaptive pp

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -21,31 +21,5 @@ function mobileBurger() {
   });
 }
 
-function loadImages() {
-  const image1 = document.querySelector('.slider__img--before');
-  const image2 = document.querySelector('.slider__img--after');
-  image1.onload = function() {
-    const aspectRatio1 = image1.naturalWidth / image1.naturalHeight;
-    setAspectRatio(1, aspectRatio1);
-  };
-
-  image2.onload = function() {
-    const aspectRatio2 = image2.naturalWidth / image2.naturalHeight;
-    setAspectRatio(2, aspectRatio2);
-  };
-}
-
-const slider = document.querySelector('.slider__range');
-const container = document.querySelector('.slider__wrapper');
-
-function setAspectRatio(imageNumber, aspectRatio) {
-  const aspectRatioString = aspectRatio.toString();
-  document.querySelector('.slider__wrapper-img').style.setProperty('--aspect-ratio', aspectRatioString);
-}
-
-slider.addEventListener('input', (e) => {
-  container.style.setProperty('--position', `${e.target.value }%`);
-});
-
 mobileBurger();
-loadImages();
+
---
+++ source/scripts/slider.js
@@ -0,0 +1,10 @@
+function loadImages() {
+  const slider = document.querySelector('.slider__range');
+  const container = document.querySelector('.slider__wrapper');
+
+  slider.addEventListener('input', (e) => {
+    container.style.setProperty('--position', `${e.target.value }%`);
+  });
+}
+
+loadImages();
