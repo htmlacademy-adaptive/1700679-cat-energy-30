9d28cd594b4e7ea15dbc55ae565e8abb1eaf0af1
Van1s1mys <malkovsin@gmail.com>
2024-01-22 11:44:11 +0500

    catalog pp

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,10 +1,6 @@
-function loadImages() {
-  const slider = document.querySelector('.slider__range');
-  const container = document.querySelector('.slider__wrapper');
+const slider = document.querySelector('.slider__range');
+const container = document.querySelector('.slider__wrapper');
 
-  slider.addEventListener('input', (e) => {
-    container.style.setProperty('--position', `${e.target.value }%`);
-  });
-}
-
-loadImages();
+slider.addEventListener('input', (e) => {
+  container.style.setProperty('--position', `${e.target.value }%`);
+});
