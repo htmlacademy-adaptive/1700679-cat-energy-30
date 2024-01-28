933fd0f98f52d4f08bc5855d3a3a68bd4e8be912
Van1s1mys <malkovsin@gmail.com>
2024-01-28 15:51:06 +0500

    pp

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -2,5 +2,5 @@ const slider = document.querySelector('.slider__range');
 const container = document.querySelector('.slider__wrapper');
 
 slider.addEventListener('input', (e) => {
-  container.style.setProperty('--position', `${e.target.value }%`);
+  container.style.setProperty('--position', `${e.target.value}%`);
 });
